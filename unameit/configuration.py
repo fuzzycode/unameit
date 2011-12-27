b4410eca075e7235a2a9e8226609eeb8c2a9d7d0
Bjorn Larsson <b.e.larsson@gmail.com>
2011-12-27 19:30:27 +0100

    Implemented the configuration parser with tests.

--- unameit/configuration.py
+++ unameit/configuration.py
@@ -16,9 +16,135 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-TODO:
+A module for reading and managing configuration files for **unameit**.
+Provides functionality to read and merge the config files and to validate the
+ correctness of the configurations.
 """
 
 import logging
+import os
+import yaml
+
+__all__ = ['Group', 'read', 'validate', 'ConfigError']
 
 logger = logging.getLogger(__name__)
+
+class ConfigError(Exception):
+    """Raised if there were errors reading a configuration file"""
+    pass
+
+
+class Group(object):
+    """
+    Holds data for a configuration group
+    """
+
+    def __init__(self, name, data):
+        self._name = name
+        self._data = data
+
+    def __getattr__(self, item):
+        try:
+            return self._data[item]
+        except KeyError:
+            raise AttributeError("Group has no attribute {0}".format(item))
+
+    def __repr__(self):
+        return "<Group {0}>".format(self._name)
+
+
+def read(files):
+    #Make sure that we have an iterable containing at least 1 file
+    if not hasattr(files, "__iter__"):
+        files = [files]
+
+    logger.debug("Processing {0} files".format(len(files)))
+
+    #Parse all files in the order provided and merge into 1 dict holding the
+    #merged content
+    file_data = dict()
+    for path in files:
+        if not os.path.isabs(path):
+            _old = path
+            path = os.path.abspath(os.path.join(os.getcwd(), path))
+            logger.debug("Making {0} absolute => {1}".format(_old, path))
+
+        path = os.path.normpath(path)
+
+        if os.path.isfile(path):
+            data = _read_file(path)
+            file_data = _merge(file_data, data)
+        else:
+            logger.info("{0} not found. Was ignored.".format(path))
+
+    #process all groups and merge with default data if required
+    default = file_data.get("default", dict())
+    #filter out the default section
+    data = dict((k, v) for k, v in file_data.items() if k != "default")
+
+    groups = list()
+    for name, data in data.items():
+        merge = data.get("use_defaults", False)
+
+        if merge:
+            groups.append(Group(name, _merge(data, default)))
+        else:
+            groups.append(Group(name, data))
+
+    return groups
+
+
+def validate(required, data):
+    pass
+
+
+def _read_file(path):
+    assert os.path.isabs(path), "Path should be absolute"
+
+    try:
+        with open(path, "rt") as config:
+            data = yaml.safe_load(config)
+    except IOError:
+        logger.error("Unable to read {0}".format(path))
+        raise ConfigError("Unable to read {0}".format(path))
+    except yaml.YAMLError as e:
+        if hasattr(e, "problem_mark"):
+            line = e.problem_mark.line + 1
+            column = e.problem_mark.column + 1
+            logger.error("Problem found in {0} at ({1} : {2})".
+            format(path, line, column))
+        else:
+            logger.error("Error found while parsing {0}".format(path))
+
+        raise ConfigError("Error parsing {0}".format(path))
+
+    return data
+
+
+def _merge(left, right):
+    """
+    :param left: Left hand side
+    :param right: Right hand side
+    :return:
+
+    Merges the left and right argument. Dictionaries are merged recursively,
+    lists are extended and made unique.
+
+    .. Note: In case of a conflict, values in *right* will overwrite values
+    in *left*.
+    """
+
+    if isinstance(left, dict) and isinstance(right, dict):
+        for key, value in list(left.items()):
+            if key not in right:
+                right[key] = value
+            else:
+                right[key] = _merge(value, right[key])
+    elif isinstance(left, list) and isinstance(right, list):
+        right.extend(left)
+        right = list(set(right))
+
+    return right
+
+
+
--- unameit/tests/test_configuration.py
+++ unameit/tests/test_configuration.py
@@ -15,3 +15,146 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import unittest
+import sys
+import os
+
+from unameit.configuration import _merge, _read_file, read, ConfigError
+
+class TestMerge(unittest.TestCase):
+    """Tests the merging of two dictionaries"""
+
+    def test_merge(self):
+        """It should be possible to merge 2 dicts"""
+        d1 = {"a": 10, "b": 20}
+        d2 = {"b": 30, "c": 40}
+
+        merged = _merge(d1, d2)
+        self.assertEqual(merged, {"a": 10, "b": 30, "c": 40})
+
+    def test_dicts(self):
+        """It should be possible to merge nested dict"""
+        d1 = {"a": {"b": 10, "c": 20}}
+        d2 = {"a": {"d": 30, "e": "foo"}}
+
+        result = _merge(d1, d2)
+        self.assertEqual(result, {"a": {"b": 10, "c": 20, "d": 30,
+                                        "e": "foo"}})
+
+    def test_lists(self):
+        """it should be possible to merge lists"""
+        l1 = [10, 4.5, 11]
+        l2 = [7, 14, 21]
+        l3 = ["hello", "world"]
+        l4 = ["foo", "bar"]
+
+        result = sorted(_merge(l1, l2))
+        self.assertEqual(result, [4.5, 7, 10, 11, 14, 21])
+
+        result = sorted(_merge(l3, l4))
+        self.assertEqual(result, ["bar", "foo", "hello", "world"])
+
+    def test_nested_lists(self):
+        """It should be possible to merge nested lists"""
+        d1 = {"a": [10, 20]}
+        d2 = {"a": [30, 40]}
+
+        result = _merge(d1, d2)
+        result["a"] = sorted(result["a"])
+
+        self.assertEqual(result, {"a": [10, 20, 30, 40]})
+
+    def test_unique_list(self):
+        """Dupes should be removed from merged lists"""
+        l1 = [20, 12, 70]
+        l2 = [4, 9, 20]
+
+        result = _merge(l1, l2)
+        self.assertEqual(sorted(result), [4, 9, 12, 20, 70])
+
+    def test_values(self):
+        """It should be possible to merge 2 values"""
+        self.assertEqual(_merge(2, 5), 5)
+        self.assertEqual(_merge("hello", "world"), "world")
+
+
+class TestReadFile(unittest.TestCase):
+    """test the reading of a single file"""
+
+    def setUp(self):
+        super(TestReadFile, self).setUp()
+        self.data_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "data"))
+
+    def test_reading(self):
+        """It should be possible to read a config file"""
+        config = os.path.join(self.data_path, "first_conf.cfg")
+        data = _read_file(config)
+
+        self.assertEqual(data["default"], {"language": "en"})
+
+    def test_yaml_error(self):
+        """
+        Function should raise ConfigError if reading an invalid config file
+        """
+        config = os.path.join(self.data_path, "invalid.cfg")
+        self.assertRaises(ConfigError, _read_file, config)
+
+    def test_read_error(self):
+        """
+        Function should raise ConfigError if the file could not be opened
+        """
+        config = os.path.join(self.data_path, "first_config.cfg")
+        self.assertRaises(ConfigError, _read_file, config)
+
+
+    def test_non_absolute_path(self):
+        """Function should raise an error if path is not absolute"""
+        self.assertRaises(AssertionError, _read_file, "first_conf.cfg")
+
+
+class TestReadFiles(unittest.TestCase):
+    """Tests the reading of multiple files"""
+
+    def setUp(self):
+        super(TestReadFiles, self).setUp()
+        self.data_path = os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "data"))
+
+    def test_single_file(self):
+        """
+        It should be possible to load a single file by providing the path as
+        a string.
+        """
+        config = os.path.join(self.data_path, "first_conf.cfg")
+        data = read(config)
+
+        self.assertEqual(len(data), 1)
+
+    def test_multiple_files(self):
+        """
+        It should be possible to load several files by providing a iterable
+        containing the paths.
+        """
+
+        config1 = os.path.join(self.data_path, "first_conf.cfg")
+        config2 = os.path.join(self.data_path, "second_conf.cfg")
+        data = read([config1, config2])
+
+        self.assertEqual(len(data), 2)
+
+    def test_invalid_files(self):
+        """Files that could not be found should be ignored when reading"""
+        config = os.path.join(self.data_path, "first_conf.cfg")
+        data = read([config, os.path.expanduser("~/conf.cfg")])
+
+        self.assertEqual(len(data), 1)
+
+    def test_relative_path(self):
+        """It should be possible to load files with a relative path"""
+        data = read("./data/first_conf.cfg")
+        self.assertEqual(len(data), 1)
+
+#Run all tests
+if __name__ == "__main__":
+    sys.exit(unittest.main())
